8cea3f638df7db7877dab595e4743e65fc84d75d
harshadasawant22@gmail.com <Panasonic@1209>
2023-04-24 03:06:25 +0530

    inital commit

---
+++ mongoApp1/src/main/java/com/example/mongoApp1/repository/ItemRepository.java
@@ -0,0 +1,23 @@
+package com.example.mongoApp1.repository;
+
+
+
+import com.example.mongoApp1.documents.GroceryItem;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends MongoRepository<GroceryItem, String> {
+
+    @Query("{name:'?0'}")
+    GroceryItem findItemByName(String name);
+
+    @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
+    List<GroceryItem> findAll(String category);
+
+    public long count();
+
+}
