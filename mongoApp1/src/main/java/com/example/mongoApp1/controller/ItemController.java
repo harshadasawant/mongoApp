e5c11581a138ae6c043c198faf1c16cbd14b038a
harshadasawant22@gmail.com <Panasonic@1209>
2023-05-05 05:34:26 +0530

    added controller

---
+++ mongoApp1/src/main/java/com/example/mongoApp1/controller/ItemController.java
@@ -0,0 +1,23 @@
+package com.example.mongoApp1.controller;
+
+import com.example.mongoApp1.documents.GroceryItem;
+import com.example.mongoApp1.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+    @Autowired
+    ItemRepository groceryItemRepo;
+
+    @GetMapping("/{item_name}")
+    public GroceryItem getProduct(@PathVariable("item_name") String itemName) {
+        GroceryItem item = groceryItemRepo.findItemByName(itemName);
+        return item;
+    }
+
+    }
